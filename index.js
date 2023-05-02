476a0d4ccdf89e0f49a1f3894070d170cb91d994
Vaibz <91090325+VaibZeus@users.noreply.github.com>
2023-05-02 09:36:47 +0530

    "first commit"

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const _ = require("lodash")
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/",(req,res)=>{
+    res.render("default");
+} )
+
+app.listen(4040, ()=>{
+    console.log("server started on port 4040");
+})
+
